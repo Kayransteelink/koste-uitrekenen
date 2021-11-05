340d4816d96f871ff3ef92538a3a2b5f9f08f2fd
Kayransteelink <33291@ma-web.nl>
2021-11-05 10:53:38 +0100

    costen

--- km.py
+++ km.py
@@ -2,9 +2,11 @@ verzekering_per_maand = 23
 benzine_kosten_per_liter = 1.54
 liter_per_kilometer = 0.2 
 
-def bereken_maandkosten():
-    km_per_maand = float(input("Hoeveel kilometers rij je per maand? "))
+def bereken_maandkosten(km_per_maand):
+    
     maandkosten = (km_per_maand * liter_per_kilometer * benzine_kosten_per_liter) + verzekering_per_maand
     print("De kosten zijn ", maandkosten, ' euro per maand.')
+    return maandkosten
 
-bereken_maandkosten()
+km_per_maand = float(input("Hoeveel kilometers rij je per maand? "))
+resultaat = bereken_maandkosten(km_per_maand)
